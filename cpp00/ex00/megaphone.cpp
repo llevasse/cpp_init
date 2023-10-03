2a6fa3d6c3009d0bc022e72a41af9d19767ca65a
llevasse <leonardlevasseur@yahoo.com>
2023-10-03 14:45:15 +0200

    LOUD AND UNBEARABLE

---
+++ cpp00/ex00/megaphone.cpp
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 14:39:49 by llevasse          #+#    #+#             */
+/*   Updated: 2023/10/03 14:44:47 by llevasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv){
+	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	(void)argc;
+	(void)argv;
+	return 0;
+}
