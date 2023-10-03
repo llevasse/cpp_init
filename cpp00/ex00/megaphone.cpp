a72a3d3ce18fd9406a1c0a993a8fa1bcac7b21db
llevasse <leonardlevasseur@yahoo.com>
2023-10-03 21:55:03 +0200

    yeah we print args

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -6,14 +6,21 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 14:39:49 by llevasse          #+#    #+#             */
-/*   Updated: 2023/10/03 14:44:47 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/10/03 21:54:35 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
 int	main(int argc, char **argv){
-	std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+	else
+	{
+		for (int i = 1; argv[i]; i++)
+			std::cout << argv[i];
+		std::cout << std::endl;
+	}
 	(void)argc;
 	(void)argv;
 	return 0;
