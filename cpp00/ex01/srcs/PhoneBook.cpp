114957ab282619435d67e4d56eb505c3a8f1b38a
Leo <leonardlevasseur@yahoo.com>
2023-10-04 15:36:23 +0200

    init phoneBook.cpp constructor

--- cpp00/ex01/srcs/PhoneBook.cpp
+++ cpp00/ex01/srcs/PhoneBook.cpp
@@ -6,8 +6,17 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 22:37:39 by llevasse          #+#    #+#             */
-/*   Updated: 2023/10/03 22:37:40 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/10/04 15:35:47 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "PhoneBook.hpp"
+
+PhoneBook::PhoneBook(){
+	std::cout << "PhoneBook constructor called!" << std::endl;
+}
+
+PhoneBook::~PhoneBook(){
+	std::cout << "PhoneBook destructor called!" << std::endl;
+}
 
