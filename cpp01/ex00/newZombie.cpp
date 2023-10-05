b2895da5b255057e2327143e74bc58a0086278f8
llevasse <leonardlevasseur@yahoo.com>
2023-10-05 22:31:28 +0200

    going home :0

--- cpp01/ex00/newZombie.cpp
+++ cpp01/ex00/newZombie.cpp
@@ -1,30 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*   newZombie.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/03 22:38:09 by llevasse          #+#    #+#             */
-/*   Updated: 2023/10/05 22:26:04 by llevasse         ###   ########.fr       */
+/*   Created: 2023/10/05 22:24:42 by llevasse          #+#    #+#             */
+/*   Updated: 2023/10/05 22:27:54 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef ZOMBIE_HPP
-# define ZOMBIE_HPP
+#include "Zombie.hpp"
 
-# include <iostream>
-
-class Zombie
-{
-	private:
-		std::string name;
-
-	public:
-		void announce( void );
-		Zombie* newZombie( std::string name );
-		void	randomChump( std::string name );
-		Zombie();
-		~Zombie();
-};
-#endif
+Zombie* Zombie::newZombie( std::string name ){
+	Zombie *greg = new Zombie;
+	greg.name = name;
+	return greg;
+}
