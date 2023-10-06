2788db5d9fc818e66a6cca04c2df6587899942bc
llevasse <leonardlevasseur@yahoo.com>
2023-10-06 22:35:22 +0200

    heuuuuuuughhhhhhhhhhhhhhhhhh

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -1,20 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   randomChump.cpp                                    :+:      :+:    :+:   */
+/*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/06 22:26:28 by llevasse          #+#    #+#             */
-/*   Updated: 2023/10/06 22:30:59 by llevasse         ###   ########.fr       */
+/*   Created: 2023/10/06 22:29:06 by llevasse          #+#    #+#             */
+/*   Updated: 2023/10/06 22:33:33 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-void	Zombie::randomChump( std::string name ){
-	Zombie chump;
-
-	chump.name = name;
-	chump.announce;
+int	main()
+{
+	Zombie::randomChump("Greg");
 }
