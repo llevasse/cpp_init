53cd937eb8cba619955f613d6e1635fafd34f197
llevasse <leonardlevasseur@yahoo.com>
2023-10-07 20:56:12 +0200

    init Weapon.cpp

---
+++ cpp01/ex03/Weapon.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/07 20:18:05 by llevasse          #+#    #+#             */
+/*   Updated: 2023/10/07 20:53:28 by llevasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Weapon.hpp"
+
+Weapon::Weapon(){
+	std::cout << "Weapon constructor called" << std::endl;
+	this.type = "hands";
+}
+
+Weapon::Weapon( std::string type ){
+	std::cout << "Weapon constructor called with arg \"" << type << "\"" << std::endl;
+	this.type = type;
+}
+
+void Weapon::setType( std::string type ){
+	this.type = type;
+}
+
+const std::string Weapon::getType(){
+	return this.type;
+}
+
+Weapon::~Weapon(){
+	std::cout << "Weapon destructor called" << std::endl;
+}
---
+++ cpp01/ex03/Weapon.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Weapon.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/03 22:38:09 by llevasse          #+#    #+#             */
+/*   Updated: 2023/10/07 20:14:29 by llevasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WEAPON_HPP
+# define WEAPON_HPP
+
+# include <iostream>
+
+class Weapon
+{
+	private:
+		std::string type;
+
+	public:
+		void setType( std::string type );
+		const std::string getType();
+		Weapon();
+		Weapon(std::string type);
+		~Weapon();
+};
+#endif
