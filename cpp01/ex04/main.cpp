51ea07faef1029f7d907d4a9fd1a9a6648e7b063
llevasse <leonardlevasseur@yahoo.com>
2023-10-08 15:57:00 +0200

    can sort of replace

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,12 +6,25 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 14:03:59 by llevasse          #+#    #+#             */
-/*   Updated: 2023/10/08 15:11:43 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/10/08 15:56:30 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
+
+std::string	replace(std::string line, std::string s1, std::string s2){
+	for(unsigned long i = 0; i < line.length(); i++){
+		if (line.compare(i, s1.length(), s1) == 0)
+		{
+			for (unsigned long j = 0; j < s2.length(); j++){
+				line[i + j] = s2[j];
+			}
+			i += s2.length();
+		}
+	}
+	return (line);
+}
 
 int	main(int argc, char **argv){
 	if (argc != 4)
@@ -23,7 +36,7 @@ int	main(int argc, char **argv){
 	std::ifstream inf(argv[1]);
 	std::string line;
 	while (getline(inf, line)){
-		outf << line << std::endl;
+		outf << replace(line, argv[2], argv[3]) << std::endl;
 	}
 	inf.close();
 	outf.close();
