09ea2474fbcb41307747db07f6cc1383cf8e9d6e
llevasse <leonardlevasseur@yahoo.com>
2023-10-08 15:12:43 +0200

    can duplicate file :0

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -6,15 +6,26 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 14:03:59 by llevasse          #+#    #+#             */
-/*   Updated: 2023/10/08 14:06:09 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/10/08 15:11:43 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <fstream>
 
 int	main(int argc, char **argv){
 	if (argc != 4)
 		{ std::cout << "Invalid number of arguments" << std::endl; return 1;}
-	(void)argv;
+	std::string const out_name = argv[1] + std::string(".replace");
+	std::ofstream	outf;
+	outf.open(out_name.c_str());
+
+	std::ifstream inf(argv[1]);
+	std::string line;
+	while (getline(inf, line)){
+		outf << line << std::endl;
+	}
+	inf.close();
+	outf.close();
 	return 0;
 }
