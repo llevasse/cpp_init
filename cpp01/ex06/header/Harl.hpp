6b944b188b62640d097843868038c2ac5631e10b
llevasse <leonardlevasseur@yahoo.com>
2023-10-08 19:47:07 +0200

    ueahhhhhhhhhhhhhhhhhhhhhhhhhhhhhh

---
+++ cpp01/ex06/header/Harl.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/08 18:29:11 by llevasse          #+#    #+#             */
+/*   Updated: 2023/10/08 19:18:04 by llevasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HARL_HPP
+# define HARL_HPP
+
+# include <iostream>
+
+class Harl{
+	private:
+		void	debug( void );
+		void	info( void );
+		void	warning( void );
+		void	error( void );
+	
+	public:
+		void	complain( std::string level );
+		Harl();
+		~Harl();
+};
+#endif
---
+++ cpp01/ex06/srcs/Harl.cpp
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Harl.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/08 18:28:59 by llevasse          #+#    #+#             */
+/*   Updated: 2023/10/08 19:46:50 by llevasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Harl.hpp"
+
+Harl::Harl(){
+	std::cout << "Harl constructor called !" << std::endl;
+}
+
+void Harl::debug(){
+	std::cout << "[ DEBUG ]" << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger." << std::endl;
+	std::cout << "I really do!" << std::endl << std::endl;
+}
+
+void Harl::info(){
+	std::cout << "[ INFO ]" << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money." << std::endl;
+	std::cout << "You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl << std::endl;
+}
+
+void Harl::warning(){
+	std::cout << "[ WARNING ]" << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free." << std::endl;
+	std::cout << "I’ve been coming for years whereas you started working here since last month." << std::endl << std::endl;
+}
+
+void Harl::error(){
+	std::cout << "[ ERROR ]" << std::endl;
+	std::cout << "This is unacceptable!" << std::endl;
+	std::cout << "I want to speak to the manager now." << std::endl << std::endl;
+}
+
+void Harl::complain( std::string level ){
+	int	i					= -1;
+	std::string	option[]	= {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for	(int j = 0; j < 4; j++){
+		if (level.compare(option[j]) == 0)
+			{i = j; break ;}
+	}
+	switch (i){
+		case 0:
+			debug();
+			info();
+			warning();
+			error();
+			break ;
+		case 1:
+			info();
+			warning();
+			error();
+			break ;
+		case 2:
+			warning();
+			error();
+			break ;
+		case 3:
+			error();
+			break ;
+		default:
+			std::cout << "[ Probably complaining ]" << std::endl;
+	}
+	
+}
+
+Harl::~Harl(){
+	std::cout << "Harl destructor called !" << std::endl;
+}
