02b189f2b753fcebbcd140aa74f82d4a3a750707
llevasse <leonardlevasseur@yahoo.com>
2023-10-09 14:32:17 +0200

    I guess it's okay ?

--- cpp02/ex00/srcs/Fixed.cpp
+++ cpp02/ex00/srcs/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 09:15:38 by llevasse          #+#    #+#             */
-/*   Updated: 2023/10/09 14:21:25 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/10/09 14:31:28 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,4 +29,16 @@ Fixed	&Fixed::operator= (const Fixed &obj){
 		this->nb = obj.nb;
 	}
 	return (*this);
+}
+
+int	Fixed::getRawBits( void ) const{
+	return (nb >> fractBits);
+}
+
+void Fixed::setRawBits( int const raw ){
+	nb = raw;
+}
+
+Fixed::~Fixed(){
+	std::cout << "Fixed point destructor called!" << std::endl;
 }
---
+++ cpp02/ex00/srcs/main.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/09 14:28:36 by llevasse          #+#    #+#             */
+/*   Updated: 2023/10/09 14:31:06 by llevasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int	main(void) {
+	Fixed a;
+	Fixed b( a );
+	Fixed c;
+
+	c = b;
+
+	std::cout << a.getRawBits() << std::endl;
+	a.setRawBits(2);
+	std::cout << a.getRawBits() << std::endl;
+	std::cout << b.getRawBits() << std::endl;
+	std::cout << c.getRawBits() << std::endl;
+}
