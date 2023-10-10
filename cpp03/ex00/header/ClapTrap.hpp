a7d6d349031a0d8ad64a880b2a0a437d39af5087
llevasse <leonardlevasseur@yahoo.com>
2023-10-10 11:28:47 +0200

    init hpp

--- cpp03/ex00/header/ClapTrap.hpp
+++ cpp03/ex00/header/ClapTrap.hpp
@@ -6,8 +6,28 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/10 11:15:32 by llevasse          #+#    #+#             */
-/*   Updated: 2023/10/10 11:15:37 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/10/10 11:28:27 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAPP_HPP
 
+# include <iostream>
+
+class ClapTrap{
+	private:
+		std::string _name;
+		int			_hp;
+		int			_energy;
+		int			_attack;
+	public:
+		ClapTrap( void );
+		ClapTrap( std::string name );
+		void	attack( const std::string& target );
+		void	takeDamage( unsigned int amount );
+		void	beRepaired( unsigned int amount );
+
+};
+
+#endif
