876a00bc13c707cbef0c0af5cb98486f2115016e
llevasse <leonardlevasseur@yahoo.com>
2023-10-10 19:53:35 +0200

    init ex02

---
+++ cpp03/ex02/header/ClapTrap.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/10 11:15:32 by llevasse          #+#    #+#             */
+/*   Updated: 2023/10/10 19:42:18 by llevasse         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap{
+	protected:
+		std::string _name;
+		int			_hp;
+		int			_energy;
+		int			_attack;
+	public:
+		ClapTrap( void );
+		ClapTrap( std::string name );
+		ClapTrap( ClapTrap const &obj);
+		ClapTrap	operator= (const ClapTrap &obj);
+		void		attack( const std::string& target );
+		void		takeDamage( unsigned int amount );
+		void		beRepaired( unsigned int amount );
+		~ClapTrap( void );
+};
+
+#endif
