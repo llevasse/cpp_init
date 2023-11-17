c35ba0792e56baad59f1c9560f5e0283d4e2ac0f
llevasse <leonardlevasseur@yahoo.com>
2023-11-17 11:02:10 +0100

    update main

--- cpp02/ex02/srcs/main.cpp
+++ cpp02/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 14:28:36 by llevasse          #+#    #+#             */
-/*   Updated: 2023/10/09 23:12:16 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/11/17 10:50:06 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,24 @@ int	main(void) {
 	Fixed		a( 42.016f);
 	Fixed 		b( 42 );
 
-	std::cout << "a is " << a << std::endl;
-	std::cout << "b is " << b << std::endl;
-	std::cout << "a > b is " << (a > b) << std::endl;
-	std::cout << "a < b is " << (a < b) << std::endl;
-	std::cout << "a >= b is " << (a >= b) << std::endl;
-	std::cout << "a <= b is " << (a <= b) << std::endl;
-	std::cout << "a == b is " << (a == b) << std::endl;
-	std::cout << "a != b is " << (a != b) << std::endl;
-	std::cout << "++a is " << ++a << std::endl;
-	std::cout << "a is " << a << std::endl;
-	std::cout << "a++ is " << a++ << std::endl;
-	std::cout << "a is " << a << std::endl;
-	std::cout << "--a is " << --a << std::endl;
-	std::cout << "a is " << a << std::endl;
-	std::cout << "a-- is " << a-- << std::endl;
-	std::cout << "a is " << a << std::endl;
-	std::cout << "min a/b " << Fixed::min(a, b) << std::endl;
-	std::cout << "max a/b " << Fixed::max(a, b) << std::endl;
+	std::cout << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "b = " << b << std::endl;
+	std::cout << "a > b = " << (a > b) << std::endl;
+	std::cout << "a < b = " << (a < b) << std::endl;
+	std::cout << "a >= b = " << (a >= b) << std::endl;
+	std::cout << "a <= b = " << (a <= b) << std::endl;
+	std::cout << "a == b = " << (a == b) << std::endl;
+	std::cout << "a != b = " << (a != b) << std::endl << std::endl;
+	std::cout << "++a = (" << ++a << ")" <<std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "a++ = (" << a++ << ")"	<< std::endl;
+	std::cout << "a = " << a << std::endl << std::endl;
+	std::cout << "--a = (" << --a << ")" <<  std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "a-- = (" << a-- << ")" << std::endl;
+	std::cout << "a = " << a << std::endl << std::endl;
+	std::cout << "min(a,b) " << Fixed::min(a, b) << std::endl;
+	std::cout << "max(a,b) " << Fixed::max(a, b) << std::endl;
+	std::cout << std::endl;
 }
