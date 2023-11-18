514434dce476e71c470762e73bf738f6a96fe97f
llevasse <leonardlevasseur@yahoo.com>
2023-11-18 18:23:15 +0100

    update main

--- cpp02/ex00/srcs/main.cpp
+++ cpp02/ex00/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/09 14:28:36 by llevasse          #+#    #+#             */
-/*   Updated: 2023/11/15 13:06:23 by llevasse         ###   ########.fr       */
+/*   Updated: 2023/11/18 18:23:01 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ int	main(void) {
 
 	c = b;
 
-	std::cout << a.getRawBits() << std::endl;
+	std::cout << "a value : " << a.getRawBits() << std::endl;
 	a.setRawBits(8);
-	std::cout << a.getRawBits() << std::endl;
-	std::cout << b.getRawBits() << std::endl;
-	std::cout << c.getRawBits() << std::endl;
+	std::cout << "a value after setRawBits(8) : " << a.getRawBits() << std::endl;
+	std::cout << "b value : " << b.getRawBits() << std::endl;
+	std::cout << "c value : " << c.getRawBits() << std::endl;
 }
