72a596fc43f2492b50c878ae37d2de6477828424
llevasse <leonardlevasseur@yahoo.com>
2024-01-01 14:02:48 +0100

    init attribute

--- cpp05/ex01/header/Form.hpp
+++ cpp05/ex01/header/Form.hpp
@@ -4,11 +4,31 @@
 # include <iostream>
 
 class Form{
+	private:
+		const std::string	_name;
+		bool				_signed;
+		const int			_signGrade;
+		const int			_execGrade;
 	public:
 		Form( void );
 		Form( Form const &obj );
+		Form( std::string name, int signGrade, int execGrade );
 		Form operator= ( Form const &obj );
 		~Form( void );
+
+		const std::string	getName();
+		bool				getSigned();
+		const int			getSignGrade();
+		const int			getExecGrade();
+
+		class GradeTooHighException: public std::exception{
+			public:
+				virtual const char	*what( void ) const throw();
+		};
+		class GradeTooLowException: public std::exception{
+			public:
+				virtual const char	*what( void ) const throw();
+		};
 };
 
 std::ostream &operator << (std::ostream &out, const Form &obj);
