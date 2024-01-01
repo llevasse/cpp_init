b35f175a02d93c123a0ad98873e82585ec408048
llevasse <leonardlevasseur@yahoo.com>
2024-01-01 15:07:36 +0100

    lil bug

--- class.sh
+++ class.sh
@@ -29,22 +29,22 @@ cd srcs
 echo "#include \"$1.hpp\"
 
 $1::$1( void ){
-	std::cout << "$1 default \033[32mconstructor\033[0m called!" << std::endl;
+	std::cout << \"$1 default "'\\'"033[32mconstructor"'\\'"033[0m called!\" << std::endl;
 }
 
 $1::$1( $1 const &obj){
-	std::cout << "$1 copy \033[32mconstructor\033[0m called!" << std::endl;
+	std::cout << \"$1 copy "'\\'"033[32mconstructor"'\\'"033[0m called!\" << std::endl;
 	if (this != &obj)
 		*this = obj;
 }
 
 $1 $1::operator= ( $1 const &obj){
-	std::cout << "$1 copy assignment operator called!" << std::endl;
+	std::cout << \"$1 copy assignment operator called!\" << std::endl;
 	return (*this);
 }
 
 $1::~$1( void ){
-	std::cout << "$1 \033[31mdestructor\033[0m called!" << std::endl;
+	std::cout << \"$1 "'\\'"033[31mdestructor"'\\'"033[0m called!\" << std::endl;
 }
 
 std::ostream &operator << (std::ostream &out, const $1 &obj){
