658eee6c72f5e27cb0f0bd99cbf661942c148b19
llevasse <leonardlevasseur@yahoo.com>
2024-01-03 22:58:21 +0100

    init robotomy

---
+++ cpp05/ex02/header/RobotomyRequestForm.hpp
@@ -0,0 +1,22 @@
+#ifndef ROBOTOMYREQUESTFORM_HPP
+# define ROBOTOMYREQUESTFORM_HPP
+
+# include <iostream>
+# include "AForm.hpp"
+
+class RobotomyRequestForm : public AForm{
+	private:
+		std::string	_target;
+	public:
+		RobotomyRequestForm( void );
+		RobotomyRequestForm( std::string target );
+		RobotomyRequestForm( RobotomyRequestForm const &obj );
+		RobotomyRequestForm &operator= ( RobotomyRequestForm const &obj );
+		~RobotomyRequestForm( void );
+		void	execute(Bureaucrat const &executor) const;
+
+		std::string	getTarget( void ) const;
+};
+
+std::ostream &operator << (std::ostream &out, const RobotomyRequestForm &obj);
+#endif
