3868deb544f840a0684fe142bc09b426b249658a
llevasse <leonardlevasseur@yahoo.com>
2024-01-04 14:31:04 +0100

    text all three forms

--- cpp05/ex02/srcs/main.cpp
+++ cpp05/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: llevasse <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/01 01:09:50 by llevasse          #+#    #+#             */
-/*   Updated: 2024/01/03 23:17:19 by llevasse         ###   ########.fr       */
+/*   Updated: 2024/01/04 14:30:37 by llevasse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,42 @@
 int main(){
 
 	try{
-		PresidentialPardonForm a;
+		ShrubberyCreationForm a;
 		Bureaucrat Elise("Elise", 3);
+		std::cout << std::endl;
 		Elise.signForm(a);
+		std::cout << std::endl;
 		Elise.executeForm(a);
+		std::cout << std::endl;
 	}
 	catch(const std::exception &e){
 		std::cerr << e.what() << std::endl;
+		std::cerr << std::endl;
+	}
+	try{
+		RobotomyRequestForm a;
+		Bureaucrat Elise("Elise", 3);
+		std::cout << std::endl;
+		Elise.signForm(a);
+		std::cout << std::endl;
+		Elise.executeForm(a);
+		std::cout << std::endl;
+	}
+	catch(const std::exception &e){
+		std::cerr << e.what() << std::endl;
+		std::cerr << std::endl;
+	}
+	try{
+		PresidentialPardonForm a;
+		Bureaucrat Elise("Elise", 3);
+		std::cout << std::endl;
+		Elise.signForm(a);
+		std::cout << std::endl;
+		Elise.executeForm(a);
+		std::cout << std::endl;
+	}
+	catch(const std::exception &e){
+		std::cerr << e.what() << std::endl;
+		std::cerr << std::endl;
 	}
 }
