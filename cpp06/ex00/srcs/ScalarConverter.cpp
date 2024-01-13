009940873b50b42b5d1cd8b794a61f3a95ab25cd
llevasse <leonardlevasseur@yahoo.com>
2024-01-13 19:56:01 +0100

    can print float !

--- cpp06/ex00/srcs/ScalarConverter.cpp
+++ cpp06/ex00/srcs/ScalarConverter.cpp
@@ -27,13 +27,27 @@ std::ostream &operator << (std::ostream &out, const ScalarConverter &obj){
 }
 
 void	printChar( char c ){
+	std::cout << "Char type passed" << std::endl;
 	std::cout << "char : '" << c << "'" << std::endl;
 	std::cout << "int : " << static_cast<int>(c) << std::endl;
 	std::cout << "float : " << static_cast<float>(c) << "f" << std::endl;
-	std::cout << "double : " << static_cast<double>(c) << std::endl;
+	std::cout << "double : " << static_cast<double>(c) << std::endl << std::endl;
+}
+
+void	printInt( std::string input ){
+	std::cout << "Int type passed" << std::endl;
+	int nb = atoi(input.c_str());
+	if (isprint(nb))
+		std::cout << "char : '" << static_cast<char>(nb) << "'" << std::endl;
+	else
+		std::cout << "char : *unprintable*" << std::endl;
+	std::cout << "int : " << nb << std::endl;
+	std::cout << "float : " << static_cast<float>(nb) << "f" << std::endl;
+	std::cout << "double : " << static_cast<double>(nb) << std::endl << std::endl;
 }
 
 void	printDouble( std::string input ){
+	std::cout << "Double type passed" << std::endl;
 	double nb = atof(input.c_str());
 	if (isprint(nb))
 		std::cout << "char : '" << static_cast<char>(nb) << "'" << std::endl;
@@ -41,10 +55,11 @@ void	printDouble( std::string input ){
 		std::cout << "char : *unprintable*" << std::endl;
 	std::cout << "int : " << static_cast<int>(nb) << std::endl;
 	std::cout << "float : " << static_cast<float>(nb) << "f" << std::endl;
-	std::cout << "double : " << nb << std::endl;
+	std::cout << "double : " << nb << std::endl << std::endl;
 }
 
 void	printFloat( std::string input ){
+	std::cout << "Float type passed" << std::endl;
 	float nb = atof(input.c_str());
 	if (isprint(nb))
 		std::cout << "char : '" << static_cast<char>(nb) << "'" << std::endl;
@@ -52,7 +67,7 @@ void	printFloat( std::string input ){
 		std::cout << "char : *unprintable*" << std::endl;
 	std::cout << "int : " << static_cast<int>(nb) << std::endl;
 	std::cout << "float : " << nb << "f" << std::endl;
-	std::cout << "double : " << static_cast<double>(nb) << std::endl;
+	std::cout << "double : " << static_cast<double>(nb) << std::endl << std::endl;
 }
 
 
@@ -61,6 +76,8 @@ void ScalarConverter::convert( std::string input ){
 	int is_double	= (input.find(".") != std::string::npos);
 	if ((input[0] < '0' || input[0] > '9') && isprint(input[0]))
 		printChar(input[0]);
+	else if (input[0] >= '0' && input[0] <= '9' && !is_double && !is_float)
+		printInt(input);
 	else if (is_double && !is_float)
 		printDouble(input);
 	else if (is_float)
