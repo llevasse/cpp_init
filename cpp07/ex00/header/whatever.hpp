11c445e769bde0c95af6a38771eddc7bb4811395
Leo <leonardlevasseur@yahoo.com>
2024-01-15 13:32:24 +0100

    cheers

---
+++ cpp07/ex00/header/whatever.hpp
@@ -0,0 +1,13 @@
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+
+# include <iostream>
+
+template <typename T> void swap(T& a, T& b){
+	T tmp;
+	tmp = a;
+	a = b;
+	b = tmp;
+}
+
+#endif
---
+++ cpp07/ex00/srcs/main.cpp
@@ -0,0 +1,9 @@
+#include "whatever.hpp"
+
+int main(){
+	int a = 10;
+	int b = 42;
+	swap(a, b);
+	std::cout << "a : " << a << std::endl;
+	std::cout << "b : " << b << std::endl;
+}
