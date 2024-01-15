8bb60b7f3eafa2d6517f6e90046c6bc66e2bd88f
Leo <leonardlevasseur@yahoo.com>
2024-01-15 13:46:39 +0100

    lil story :3

--- cpp07/ex00/srcs/main.cpp
+++ cpp07/ex00/srcs/main.cpp
@@ -12,11 +12,12 @@ int main(){
 	}
 	{
 		char a = 'b';
-		char b = 'a';
-		swap(a, b);
+		char b = 'a';	//ho no, I fucked up the order, what ever shall I do :0
+		swap(a, b);		//worry not, the template work for chars too :D
 		std::cout << "a : " << a << std::endl;
 		std::cout << "b : " << b << std::endl;
 		std::cout << "min : " << min(a, b) << std::endl;
 		std::cout << "max : " << max(a, b) << std::endl;
+						//no everything is in order, cheers :)
 	}
 }
