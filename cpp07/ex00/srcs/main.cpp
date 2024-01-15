b945bdb988c191fa3e10fc574882df0c71cb511c
Leo <leonardlevasseur@yahoo.com>
2024-01-15 13:43:47 +0100

    cheers

--- cpp07/ex00/srcs/main.cpp
+++ cpp07/ex00/srcs/main.cpp
@@ -1,9 +1,22 @@
 #include "whatever.hpp"
 
 int main(){
-	int a = 10;
-	int b = 42;
-	swap(a, b);
-	std::cout << "a : " << a << std::endl;
-	std::cout << "b : " << b << std::endl;
+	{
+		int a = 10;
+		int b = 42;
+		swap(a, b);
+		std::cout << "a : " << a << std::endl;
+		std::cout << "b : " << b << std::endl;
+		std::cout << "min : " << min(a, b) << std::endl;
+		std::cout << "max : " << max(a, b) << std::endl;
+	}
+	{
+		char a = 'b';
+		char b = 'a';
+		swap(a, b);
+		std::cout << "a : " << a << std::endl;
+		std::cout << "b : " << b << std::endl;
+		std::cout << "min : " << min(a, b) << std::endl;
+		std::cout << "max : " << max(a, b) << std::endl;
+	}
 }
