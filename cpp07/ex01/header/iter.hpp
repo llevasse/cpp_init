9a36369667f9b2bab2eeaec45add0293dbd6b508
llevasse <leonardlevasseur@yahoo.com>
2024-01-17 12:42:16 +0100

    works for consts

--- cpp07/ex01/header/iter.hpp
+++ cpp07/ex01/header/iter.hpp
@@ -8,4 +8,10 @@ template <typename T, typename S, typename F> void iter(T *arr, S size, F (*f)(T
 		std::cout << "Return of function with arg " << arr[i] << " : " << f(arr[i]) << std::endl;
 	}
 }
+
+template <typename T, typename S, typename F> void iter(T const *arr, S const size, F (*f)(T)){
+	for (S i = 0;i < size;i++){
+		std::cout << "Return of function with arg " << arr[i] << " : " << f(arr[i]) << std::endl;
+	}
+}
 #endif
--- cpp07/ex01/srcs/main.cpp
+++ cpp07/ex01/srcs/main.cpp
@@ -6,5 +6,10 @@ int	diffTo42(int nb){
 
 int main(){
 	int	arr[] = {1,2,3,6,12,42};
+	const int	carr[] = {1,2,3,6,12,42};
+	const int	size = 6;
+
 	iter(arr, 6, diffTo42);	
+	std::cout << std::endl;
+	iter(carr, size, diffTo42);	
 }
