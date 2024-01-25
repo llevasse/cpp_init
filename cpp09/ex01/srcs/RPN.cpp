a16d7897d928c25397d87fd508d487d861b4cacf
Leo <leonardlevasseur@yahoo.com>
2024-01-25 15:45:38 +0100

    idk :(

---
+++ cpp09/ex01/srcs/RPN.cpp
@@ -0,0 +1,41 @@
+#include "RPN.hpp"
+
+RPN::RPN( void ){
+	if (!MUTE)
+		std::cout << "RPN default \033[32mconstructor\033[0m called!" << std::endl;
+}
+
+RPN::RPN( std::string operands ){
+	if (!MUTE)
+		std::cout << "RPN default \033[32mconstructor\033[0m called!" << std::endl;
+}
+
+RPN::RPN( RPN const &obj){
+	if (!MUTE)
+		std::cout << "RPN copy \033[32mconstructor\033[0m called!" << std::endl;
+	if (this != &obj)
+		*this = obj;
+}
+
+RPN &RPN::operator= ( RPN const &obj){
+	if (!MUTE)
+		std::cout << "RPN copy assignment operator called!" << std::endl;
+	if (this != &obj)
+		this->_stack = obj.getStack();	
+	return (*this);
+}
+
+RPN::~RPN( void ){
+	if (!MUTE)
+		std::cout << "RPN \033[31mdestructor\033[0m called!" << std::endl;
+}
+
+std::stack<int>	RPN::getStack( void ){
+	return (_stack);
+}
+
+std::ostream &operator << (std::ostream &out, const RPN &obj){
+	out << "RPN";
+	(void)obj;
+	return (out);
+}
