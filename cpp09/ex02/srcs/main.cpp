b38bf617dc990e4e8fc9f293e3047c8b0fa83a21
llevasse <leonardlevasseur@yahoo.com>
2024-01-29 21:04:40 +0100

    added error message if no input

--- cpp09/ex02/srcs/main.cpp
+++ cpp09/ex02/srcs/main.cpp
@@ -1,7 +1,7 @@
 #include "PmergeMe.hpp"
 
 int main(int argc, char **argv){
-	if (argc != 2){
+	if (argc == 1){
 		std::cout << "Error, correct input would be : './PmergeMe \"[A LIST OF ANY NUMBER SEPARATED BY SPACES]\"'" << std::endl;
 		return (1);
 	}
