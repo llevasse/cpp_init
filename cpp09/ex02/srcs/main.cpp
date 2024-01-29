bb57cd24b4d7d99ee25b75eb81922183185d089e
llevasse <leonardlevasseur@yahoo.com>
2024-01-29 21:02:49 +0100

    added error message if no input

--- cpp09/ex02/srcs/main.cpp
+++ cpp09/ex02/srcs/main.cpp
@@ -1,6 +1,10 @@
 #include "PmergeMe.hpp"
 
 int main(int argc, char **argv){
+	if (argc != 2){
+		std::cout << "Error, correct input would be : './PmergeMe \"[A LIST OF ANY NUMBER SEPARATED BY SPACES]\"'" << std::endl;
+		return (1);
+	}
 	PmergeMe merge;
 
 	double	vecTime = merge.sortVector(argc, argv);
@@ -8,4 +12,5 @@ int main(int argc, char **argv){
 
 	std::cout << "Vector sorting time of " << (argc - 1) << " elements : " << vecTime << std::endl;
 	std::cout << "Deque sorting time of " << (argc - 1) << " elements : " << deqTime << std::endl;
+	return (0);
 }
