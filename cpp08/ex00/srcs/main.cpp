58608f33731698a36ffeae4349a5e54e54a7c1cf
llevasse <leonardlevasseur@yahoo.com>
2024-02-06 14:24:35 +0100

    added test

--- cpp08/ex00/srcs/main.cpp
+++ cpp08/ex00/srcs/main.cpp
@@ -1,15 +1,29 @@
 #include "easyFind.hpp"
 #include <vector>
+#include <deque>
 
 int main(){
 	std::vector<int> vec;
+	std::deque<int>	deq;
 
-	for (int i=0; i < 6;i++){
+	std::cout << "Vector : ";
+	for (int i=0; i < 10;i++){
 		if (i == 3)
 			vec.push_back(42);
 		else
 			vec.push_back(i);
+		std::cout << vec[i] << " ";
 	}
+	std::cout << std::endl <<  "Position of 42 = " << easyFind(vec, 42) << std::endl;
 	
-	std::cout << easyFind(vec, 42) << std::endl;
+	std::cout << "Deque : ";
+	for (int i=0; i < 10;i++){
+		if (i == 5)
+			deq.push_back(42);
+		else
+			deq.push_back(i);
+		std::cout << deq[i] << " ";
+	}
+	std::cout << std::endl <<  "Position of 42 = " << easyFind(deq, 42) << std::endl;
+
 }
