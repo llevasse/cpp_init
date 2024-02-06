66a0358ecbe3891b99bc1f9b7158e8f70ff1965e
llevasse <leonardlevasseur@yahoo.com>
2024-02-06 14:42:00 +0100

    take arguments

--- cpp09/ex00/srcs/main.cpp
+++ cpp09/ex00/srcs/main.cpp
@@ -1,8 +1,16 @@
 #include "BitcoinExchange.hpp"
 
-int main(){
+int main(int argc, char **argv){
 	BitcoinExchange btc;
-	std::ifstream	inf("input.txt");
+	if (argc < 2){
+		std::cerr << "Error : not enough arguments" << std::endl;
+		return (1);
+	}
+	std::ifstream	inf(argv[1]);
+	if (!inf.is_open()){
+		std::cerr << "Error : can't open file" << std::endl;
+		return (1);
+	}
 	std::string		line;
 	getline(inf, line);
 	while (getline(inf, line))
