d5a0d8b81c5baf43cd129dce9d571931638924ac
llevasse <leonardlevasseur@yahoo.com>
2024-03-10 13:39:35 +0100

    added error message

--- cpp09/ex01/srcs/main.cpp
+++ cpp09/ex01/srcs/main.cpp
@@ -4,4 +4,6 @@ int main(int argc, char **argv){
 	if (argc == 2){
 		RPN rpn(argv[1]);
 	}
+	else
+		std::cout << "Error : program should be used as './RPN \"[numbers and operators]\"'" << std::endl; 
 }
